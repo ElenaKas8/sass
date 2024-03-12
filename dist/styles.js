c92dbd0df9ed229435b2bd1ad520b0ebf3db60a9
Elena <149805926+ElenaKas8@users.noreply.github.com>
2024-03-12 14:21:11 +0100

    onclick

--- dist/styles.js
+++ dist/styles.js
@@ -1,1 +1,27 @@
-console.log('Sass');
+let wrapper = document.querySelector('.wrapper');
+let big = document.querySelector('.big');
+let middle = document.querySelector('.middle');
+let small = document.querySelector('.small');
+
+
+
+
+big.addEventListener('click', function(e){
+    e.stopPropagation();
+    console.log('big');
+    
+});
+
+middle.addEventListener('click', function(e){
+    e.stopPropagation();
+    console.log('middle');
+    
+});
+
+
+small.addEventListener('click', function(e){
+    e.stopPropagation();
+    console.log('small');
+})
+
+
